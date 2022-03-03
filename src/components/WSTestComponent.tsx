757fc108cfc02a31b8ee4e3a5e0144eb7f232733
KOKI9861 <kokiota1998@gmail.com>
2022-03-04 02:17:10 +0900

    ws test

--- src/components/WSTestComponent.tsx
+++ src/components/WSTestComponent.tsx
@@ -1,28 +1,35 @@
+import { useState } from "react";
 import Socket from "../utils/webSocket";
 
 export default function WSTestComponent() {
-    let socket_data: object = {};
+    const [socketData, setSocketData] = useState({message: "init"});
 
     const URL = process.env.REACT_APP_WEBSOCKET_URL;
-    // const ws = new WebSocket(URL+"/");
-    // let socket = new Socket(ws);
+    const ws = new WebSocket(URL+"");
+    let socket = new Socket(ws);
 
-    // socket.on("data", receiveData);
+    socket.on("message", receiveData);
     function receiveData(e:any) {
         let data = JSON.parse(e.data);
         console.log(data);
-        socket_data = data;
+        setSocketData(data);
     }
 
     function handleClick() {
-        // socket.emit(({message: "Hello"}));
+        let message = {
+            messagetype: "message",
+            message: "Hello"
+        }
+        const data = JSON.stringify(message);
+        console.log(data);
+        socket.emit(data);
     }
 
     return (
         <div>
             <h1>WebSocket Test</h1>
-            {/* <p>{socket_data}</p> */}
-            <button onClick={() => (handleClick)}>Send</button>
+            <p>{socketData.message}</p>
+            <button onClick={() => handleClick()}>Send</button>
         </div>
     );
 }
