d80f0607074c26d6c02a9e8889f8c6c719b511c3
Koki Ota <ota-koki-gy@ynu.jp>
2022-03-03 11:41:02 +0900

    add api function

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -28,13 +28,17 @@ async function get(path:string) {
 
 
 export function signup(userId:string, userName:string, userPassword:string){
+    // TODO: 
+    // - ID被りのレスポンス
+    // - レスポンスのフォーマット
     const data = {
         userId: userId,
         userName: userName,
         userPassword: userPassword,
     }
     
-    post("/user/signup", data);
+    const response = post("/user/signup", data);
+    return response
 }
 
 export function login(userId:string, userPassword:string){
@@ -43,5 +47,52 @@ export function login(userId:string, userPassword:string){
         userPassword: userPassword,
     }
     
-    post("/user/login", data);
+    const response = post("/user/login", data);
+    return response;
+}
+
+export function meetingCreate(meetingName:string, startTime:Date, presenters:string[]){
+    // TODO: 
+    // - startTimeのフォーマット
+    const data = {
+        meetingName: meetingName,
+        startTime: startTime,
+        presenters: presenters,
+    }
+
+    const response = post("/meeting/create", data);
+    return response;
+}
+
+export function meetingJoin(userId:string, meetingId:string, choko:boolean=false){
+    // TODO:
+    // - startTimeのフォーマット
+    // - return のフォーマット
+    const data = {
+        userId: userId,
+        meetingId: meetingId,
+        choko: choko,
+    }
+
+    const response = post("/meeting/join", data);
+    return response;
+}
+
+export function postDocument(userId:string, meetingId:string, file:string="", script:string=""){
+    // TODO:
+    // - fileの扱い(ファイルのパスを受け取って，ここでBase64エンコードするか)
+    const data = {
+        userId: userId,
+        meetingId: meetingId,
+        file: file,
+        script: script,
+    }
+
+    const response = post("/document", data);
+    return response;
+}
+
+export function getDocument(documentId:string){
+    const response = get("/document/?id=" + documentId);
+    return response;
 }
