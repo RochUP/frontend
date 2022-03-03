0534aa8c2e13b798c0cdbf4152a4184c65fe1077
GloUnknown <gloun342708728@gmail.com>
2022-03-03 14:25:45 +0900

    ログイン、登録ページ作成完了、機能未実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,22 @@ import './App.css';
 
 import {BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
 
-// import TestPage from './pages/TestPage';
+import TestPage from './pages/TestPage';
 import LoginPage from './pages/user/loginPage';
+import RegisterPage from './pages/user/register';
 
 function App() {
   return (
-    // <div className="App">
+    <div className="App">
       <Router>
         <Routes>
-          {/* <Route path="/" element={<TestPage />} /> */}
-          {/* <Route path="/test" element={<TestPage />} /> */}
+          <Route path="/test" element={<TestPage />} />
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage/>} />
         </Routes>
       </Router>
-    // </div>
+    </div>
   );
 }
 
--- src/pages/user/loginPage.tsx
+++ src/pages/user/loginPage.tsx
@@ -1,65 +1,63 @@
-import { LoginForm, ProFormText, } from '@ant-design/pro-form';
+import { ProFormText, } from '@ant-design/pro-form';
 import {
   UserOutlined,
   LockOutlined,
 } from '@ant-design/icons';
 import '@ant-design/pro-form/dist/form.css';
+import { Button, Space,} from 'antd';
+import { Link } from 'react-router-dom';
 
 export default function LoginPage () {
-  return (
-    <div style={{ backgroundColor: 'white' }}>
-      <LoginForm
-        title="○○システム"
-        subTitle="ログイン"
-      >
-        <>
-        <ProFormText
-            name="username"
-            fieldProps={{
-            size: 'large',
-            prefix: <UserOutlined className={'prefixIcon'} />,
-            }}
-            placeholder={'ユーザネーム: admin or user'}
-            rules={[
-            {
-                required: true,
-                message: 'ユーザネームを入力してください!',
-            },
-            ]}
-        />
-        <ProFormText.Password
-            name="password"
-            fieldProps={{
-            size: 'large',
-            prefix: <LockOutlined className={'prefixIcon'} />,
-            }}
-            placeholder={'パスワード: ant.design'}
-            rules={[
-            {
-                required: true,
-                message: 'パスワードを入力してください!',
-            },
-            ]}
-        />
-        </>
-        
-        <div
-          style={{
-            marginBottom: 24,
-          }}
-        >
-          {/* <ProFormCheckbox noStyle name="autoLogin">
-            自動ログイン
-          </ProFormCheckbox> */}
-          {/* <a
-            style={{
-              float: 'right',
-            }}
-          >
-            新規登録
-          </a> */}
+    return (
+        <div className='content___2zk1-'>
+            <div className='ant-pro-form-login-container'>
+                <div className='ant-pro-form-login-container'>
+                    <div className='ant-pro-form-login-top'>
+                        <div className='ant-pro-form-login-header'>
+                            <span className='ant-pro-form-login-header-title'>
+                                ○○システム
+                            </span>
+                        </div>
+                        <div className='ant-pro-form-login-desc'>ログイン</div>
+                    </div>
+                        <div className='ant-pro-form-login-main'>
+                            <ProFormText
+                                name="username"
+                                fieldProps={{
+                                size: 'large',
+                                prefix: <UserOutlined className={'prefixIcon'} />,
+                                }}
+                                placeholder={'ユーザネーム: admin or user'}
+                                rules={[
+                                {
+                                    required: true,
+                                    message: 'ユーザネームを入力してください!',
+                                },
+                                ]}
+                            />
+                            <ProFormText.Password
+                                name="password"
+                                fieldProps={{
+                                size: 'large',
+                                prefix: <LockOutlined className={'prefixIcon'} />,
+                                }}
+                                placeholder={'パスワード: ant.design'}
+                                rules={[
+                                {
+                                    required: true,
+                                    message: 'パスワードを入力してください!',
+                                },
+                                ]}
+                            />
+                            <Space direction='vertical' style={{ width: 330 }}>
+                                <Button type='primary' block>ログイン</Button>
+                                <Link to={'../register'}>
+                                    <Button type='link' block>新規登録</Button>
+                                </Link>
+                            </Space>
+                        </div>
+                </div>
+            </div>
         </div>
-      </LoginForm>
-    </div>
-  );
+    );
 };
