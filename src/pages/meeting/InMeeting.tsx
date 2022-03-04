40b67e50f7dd9c5a8a2ad2bafed41101faa01397
GloUnknown <gloun342708728@gmail.com>
2022-03-04 18:21:44 +0900

    ミーテイング中ページフレーム完成、機能未実装、デザインdetail修正必要

--- src/pages/meeting/InMeeting.tsx
+++ src/pages/meeting/InMeeting.tsx
@@ -1,12 +1,23 @@
-import { Breadcrumb, Layout, Menu } from "antd";
+import { Breadcrumb, Button, Card, Col, Divider, Input, Layout, Menu, Row, Space, Tabs } from "antd";
 import {
-    UserOutlined
+    UserOutlined,
+    ArrowUpOutlined,
+    CommentOutlined
   } from '@ant-design/icons';
 import SubMenu from "antd/lib/menu/SubMenu";
 import { Typography } from 'antd';
 import "../../assets/css/Pages.css";
+import { Link } from "react-router-dom";
 
 const { Header, Footer, Content } = Layout;
+
+const { Text } = Typography;
+
+const { TabPane } = Tabs;
+
+function callback(key: any) {
+    console.log(key);
+}
 
 export default function InMeeting() {
 
@@ -30,7 +41,55 @@ export default function InMeeting() {
                     <Breadcrumb.Item>会議</Breadcrumb.Item>
                     <Breadcrumb.Item>会議中</Breadcrumb.Item>
                 </Breadcrumb>
-                <div className="site-layout-content" style={{background: '#fff'}}>Content</div>
+                <div className="site-layout-content" style={{background: '#fff'}}>
+                    <Row>
+                        <Col span={12} style={{background:'#DD2248'}}>
+                            <Title level={3}>○○会議進行中</Title>
+                            <Space direction="vertical">
+                                <Text type="secondary">会議ID:</Text>
+                                <div className="meeting-control">
+                                    <Link to={'../meeting/join'}>
+                                        <Button type="primary" style={{marginRight:'10px'}}>退出</Button>
+                                    </Link>
+                                </div>
+                            </Space>
+                        </Col>
+                        <Col span={12} style={{background:'#DD2248'}}>
+                            <Title level={4}>コメント一覧</Title>
+                        </Col>
+                        <Divider />
+                        {/* 左側のコンポーネント */}
+                        <Col span={12} style={{background:'#DD2248', padding:"8px 0", margin:'8px'}}>
+                            <Card style={{ width: '100%', minHeight: 300 }}>
+                                <Title level={4}>ここは司会メッセージ</Title>
+                                <Divider />
+                                This area for slide show
+                            </Card>
+                        </Col>
+                        {/* 右側のコンポーネント */}
+                        <Col span={11} style={{background:'#DD2248', padding:"8px 0", margin:'8px'}}>
+                            <Card style={{ width: '100%', minHeight: 300 }}>
+                                <Tabs defaultActiveKey="1" onChange={callback}>
+                                    <TabPane tab="発表者1" key="1">
+                                        This area for comment to presenter No.1
+                                    </TabPane>
+                                    <TabPane tab="発表者2" key="2">
+                                        This area for comment to presenter No.2
+                                    </TabPane>
+                                </Tabs>
+                            </Card>    
+                        </Col>
+                        {/* 左側操作エリア */}
+                        <Col span={12} style={{background:'#DD2248', padding:"8px 0", margin:'8px'}}>
+                            <Button type="primary" icon={<ArrowUpOutlined />} style={{width:'100%'}}>Hands up</Button>
+                        </Col>
+                        {/* 右側操作エリア */}
+                        <Col span={11} style={{background:'#DD2248', padding:"8px 0", margin:'8px'}}>
+                            <Input placeholder="ここでコメントを書いてください" style={{width:'80%'}}></Input>
+                            <Button type="primary" icon={<CommentOutlined />} style={{width:'20%'}}>Comment</Button>
+                        </Col>
+                    </Row>
+                </div>
             </Content>
             <Footer style={{ 
                 borderTop: '1px solid #e8e8e8',
