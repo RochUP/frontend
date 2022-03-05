8da3e270ae85b3ce4400b3294d80120a88899ba3
KOKI9861 <kokiota1998@gmail.com>
2022-03-05 11:33:31 +0900

    音声キーワード判定

--- src/components/testComponents/SpeechRecTestComponent.tsx
+++ src/components/testComponents/SpeechRecTestComponent.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const Dictaphone = () => {
+
+    const [ message, setMessage ] = React.useState('');
+
+    const commands = [
+        {
+            command: "*発表を終わ*",
+            callback: () => {setMessage('発表終了')}
+        },
+        {
+            command: "*質問を終わ*",
+            callback: () => {setMessage('質問終了')}
+        }
+    ]
+
     const {
         transcript,
         listening,
         resetTranscript,
         browserSupportsSpeechRecognition
-    } = useSpeechRecognition();
+    } = useSpeechRecognition({ commands });
 
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
@@ -20,6 +34,7 @@ const Dictaphone = () => {
         <button onClick={SpeechRecognition.stopListening}>Stop</button>
         <button onClick={resetTranscript}>Reset</button>
         <p>{transcript}</p>
+        <p>{message}</p>
         </div>
     );
 };
