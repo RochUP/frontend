3b36706a7010dce85ceb94f53451dc5c7c06329b
KOKI9861 <kokiota1998@gmail.com>
2022-03-08 01:29:41 +0900

    scriptを得るuseSelector追記

--- src/components/meeting/DocumentComponent.tsx
+++ src/components/meeting/DocumentComponent.tsx
@@ -1,10 +1,21 @@
 import { Card, Space } from "antd";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import PdfViewerComponent from "../../components/meeting/PdfViewerComponent";
 
 
 export default function DocumentComponent() {
 
+    const documentIds = useSelector((state: any) => state.meetingReducer.documentIds);
+    const documentIdNow = useSelector((state: any) => state.meetingReducer.documentIdNow);
+    const scripts = useSelector((state: any) => state.meetingReducer.scripts);
+    
+    const [script, setScript] = useState("");
+
+    useEffect(()=>{
+        setScript(scripts[documentIds.indexOf(documentIdNow)]);
+    },[documentIdNow]);
 
     return (
         <Space direction="horizontal" style={{maxHeight: 500, width:'100%'}}>
